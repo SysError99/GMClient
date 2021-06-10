147a2035e61be157ca3fdbb356c5765191fe072d
SysError99 <17522480+SysError99@users.noreply.github.com>
2021-06-11 05:07:41 +0700

    Updated details, improved conventions.

--- README.md
+++ README.md
@@ -1,2 +1,174 @@
 # GMClient
 An extension made for enhancing GameMaker:Studio 1.4 features and prolong its lifetime.
+---
+
+# Usage
+Compress this project as `7-Zip` archive, then change its extension from `.7z` to `.gmez`.
+
+Import file to the project and start using it!
+
+If you want to use this extension for networking via WebSocket, just change network location of `webSocketAddress` inside `./client/client.js` to your server address.
+---
+
+# References
+There are plenty of command you can use in GML, as shown below:
+
+### Audio
+This category indicates functions related to media audio player.
+1. `audio_play(url_file_location:string)` Plays audio as `Media`. (any medias or this can be interrupted by this or another media)
+2. `audio_stop()` Stops currently playing audio media.
+
+### Browser
+This category indicates functions related to dialogs, necessary if this game is running via SysError99's WebApp
+1. `dialog_alert(text:string)` Shows native dialog. This returns void.
+2. `dialog_confirm(text:string ,hint:string)` Shows confirmation dialog. This returns text. But if this is 
+3. `dialog_prompt(text:string, default:string)` Shows prompt dialog.
+4. `full_screen()` Triggers canvas full-screen mode for the canvas. Returns 1 (number) if full-screen is successful, and 0 (number) if failed.
+
+### Local Storage
+This category indicates functions for load and save strings into browser's local storage (Can be used with SysError99's WebApp)
+1. `load_data(location:string)` Load file from storage. Returns string.
+2. `save_data(location:string, data:string)` Save string to localstorage.
+
+### Networking
+This category indicates functions for communicating with a web server via WebSocket.
+1. `network_open()` Connect to a server with an address specified inside of file `./client/client.js`.
+2. `network_receive()` Retreives any message that server have sent to the client. You should put this command inside `Step` loop to continuously retrieve anything from the server or connection status. Returns empty string when there is nothing happend. Returns connection status when there is a report of connection status. Returns string of data when there is some data received from a server.
+#### Connection Status
+ - `connected` Happens when a connection is established.
+ - `error` Happens when there is an error occured.
+ - `close` Happens when a connection is closed.
+Example:
+```javascript
+    // Put this inside 'Step' Event!
+    var received_message = network_receive();
+
+    if (received_message != "") {
+        switch (received_message) {
+            case "connected":
+                show_message("Connected to a server.");
+                break;
+            case "error":
+                show_message("An error occured to the connection.");
+                break;
+            case "close":
+                show_message("Connection is closed");
+                break;
+            default:
+                show_message("This is a message from server:" + received_message);
+                break;
+        }
+    }
+```
+3. `network_send(message:string)` Send a message to a server.
+
+### SysError99's WebApp related functions
+This category indicates only one function. `is_app()` checks if this app is running inside SysError's WebApp. Returns 1 if it is, and 0 if it's not.
+
+### Multi-Touch
+This category indicates multi-touch functions.
+Example:
+```javascript
+// Put this function inside 'Step' Event~
+touch_invoke(room_width,room_height); //start touch instance with defined screen resolution.
+
+var touch_start_id;
+var touch_end_id;
+var touch_move_id;
+
+do {
+    // When Touch begins, like mouse_check_button_pressed()
+    touch_start_id = touch_start();  //get ID of each touch
+
+    if(touch_start_id > -1){ //if touch exists
+        // Usage Example
+        draw_sprite(spr_test, 1, touch_x(touch_start_id), touch_y(touch_start_id));
+        draw_set_font(font_default);
+        draw_text_ext(touch_x(touch_start_id),touch_y(touch_start_id),string(touch_start_id), 60, 200);
+    }
+} until (touch_start_id < 0);
+
+
+do {
+    // When Touch ends, like mouse_check_button_released()
+    touch_end_id = touch_end(); //get ID of each touch
+
+    if(touch_end_id > -1){
+        // Usage Example
+        draw_sprite(spr_test, 2, touch_x(touch_end_id), touch_y(touch_end_id));
+        draw_set_font(font_default);
+        draw_text_ext(touch_x(touch_end_id), touch_y(touch_end_id), string(touch_end_id), 60, 200);
+    }
+} until (touch_end_id < 0);
+
+do {
+    // When Touch continuously happens (holding), like mouse_check_button()
+    touch_move_id = touch_move(); //get ID of each touch
+
+    if(touch_move_id > -1){
+        // Usage Example
+        draw_sprite(spr_test,0,touch_x(touch_move_id),touch_y(touch_move_id));
+        draw_set_font(font_default);
+        draw_text_ext(touch_x(touch_move_id), touch_y(touch_move_id), string(touch_move_id), 60, 200);
+    }
+} until (touch_move_id < 0);
+
+touch_clean(); //end touch instance.
+
+```
+#### Limitations
+This extenstion only works with only `one instance`. If you want to use it in multiple instances, I'd recommend using `with` statement to call each instances you want instead:
+```javascript
+touch_invoke(room_width,room_height); //start touch instance with defined screen resolution.
+
+var touch_start_id;
+var touch_end_id;
+var touch_move_id;
+
+do {
+    touch_start_id = touch_start();
+    if(touch_start_id > -1){
+        // Set global values for instances.
+        global.touch_id = touch_start_id;
+        global.touch_x = touch_x(touch_start_id);
+        global.touch_y = touch_y(touch_start_id);
+        // Call instances
+        with(obj_joystick){
+            event_perform("ev_mouse","ev_left_press");
+            // then use global variables
+        }
+    }
+} until (touch_start_id < 0);
+
+do {
+    touch_end_id = touch_end();
+    if(touch_end_id > -1){
+        // Set global values for instances.
+        global.touch_id = touch_start_id;
+        global.touch_x = touch_x(touch_start_id);
+        global.touch_y = touch_y(touch_start_id);
+        // Call instances
+        with(obj_joystick){
+            event_perform("ev_mouse","ev_left_release");
+        }
+    }
+} until (touch_end_id < 0);
+
+do {
+    touch_move_id = touch_move();
+    if(touch_move_id > -1){
+        // Set global values for instances.
+        global.touch_id = touch_start_id;
+        global.touch_x = touch_x(touch_start_id);
+        global.touch_y = touch_y(touch_start_id);
+        // Call instances
+        with(obj_joystick){
+            event_perform("ev_mouse","ev_left_button);
+        }
+    }
+} until (touch_move_id < 0);
+
+touch_clean(); //end touch instance.
+
+```
+
---
+++ client/client.js
@@ -0,0 +1,439 @@
+
+/**
+ * Audio variable
+ */
+var audio = null;
+/**
+ * Network events
+ */
+ var events = [];
+/**
+ * Full screen parameter.
+ */
+var fullScreenObj = {lock: false};
+/**
+ * Network socket
+ */
+var network = null;
+/**
+ * Touch events.
+ */
+ var touch = {
+	canvas: undefined,
+	roomWidth: 1,
+	roomHeight: 1,
+	loop: -1
+};
+var touches = [];
+var touch_log = {};
+/*
+ * Server IP Address
+ */
+var webSocketAddress = "ws://192.168.1.200:8080";
+
+// - - - configure DPI on-the-fly
+var viewport = document.querySelector("meta[name=viewport]");
+
+viewport.setAttribute("content", "user-scalable=no, initial-scale=0.75, maximum-scale=1, minimum-scale=0.75, width=device-width, height=device-height, target-densitydpi=device-dpi");
+
+// - - - - - - - - - - Audio - - - - - - - - - -
+/**
+ * Play audio file.
+ * @param {string} file Audio file location.
+ */
+ function audioPlay(file) {
+	audio = new Audio(file);
+	audio.loop = true;
+	audio.play();
+}
+
+/**
+ * Stop audio play.
+ */
+function audioStop() {
+	if(audio!=null) {
+		audio.pause();
+		audio.currentTime = 0;
+	}
+}
+
+// - - - - - - - - - - Browser - - - - - - - - - -
+/**
+ * Show system alert dialog
+ * @param {string} str text string
+ */
+ function dialogAlert(str) {
+	if (isApp() === 0) {
+		fullScreenObj.lock = true;
+		closeFullScreen();
+		alert(str);
+		fullScreenObj.lock = false;
+		fullScreen();
+	} else
+		App.alert(str);
+}
+
+/**
+ * Show confirmation dialog
+ * @param {string} name Name of data to be returned.
+ * @param {string} str String of hint text
+ */
+function dialogConfirm(name,str) {
+	var input;
+
+	if (isApp() === 0) {
+		fullScreenObj.lock = true;
+		closeFullScreen();
+
+		input = prompt(str);
+
+		fullScreenObj.lock = false;
+		fullScreen();
+	} else {
+		App.confirm(name,str);
+		input = "";
+	}
+}
+
+/**
+ * Show system prompt dialog.
+ * @returns String of result or empty string.
+ */
+function dialogPrompt(name, str) {
+	var input;
+
+	if (isApp() === 0) {
+		fullScreenObj.lock = true;
+		closeFullScreen();
+		input = prompt(str);
+		fullScreenObj.lock = false;
+		fullScreen();
+	} else {
+		App.prompt(name,str);
+		input = "";
+	}
+
+	return input;
+}
+
+/**
+ * Go fullscreen!
+ * @returns {number} is it fullscreen?
+ */
+ function fullScreen() {
+	if (isApp() === 0) {
+		if (!isFullScreen() && !fullScreenObj.lock) {
+			var canvas = document.getElementById("canvas");
+
+			if (canvas.requestFullscreen)
+				canvas.requestFullscreen();
+			else if (canvas.mozRequestFullScreen)
+				canvas.mozRequestFullScreen();
+			else if (canvas.webkitRequestFullscreen)
+				canvas.webkitRequestFullscreen();
+			else if (canvas.msRequestFullscreen)
+				canvas.msRequestFullscreen();
+
+            return isFullScreen();
+		} else
+            return 1;
+	}
+
+    return 0;
+}
+
+/**
+ * Exit full screen
+ */
+function closeFullScreen() {
+	if (document.exitFullscreen)
+		document.exitFullscreen();
+	else if (document.mozCancelFullScreen)
+		document.mozCancelFullScreen();
+	else if (document.webkitExitFullscreen)
+		document.webkitExitFullscreen();
+	else if (document.msExitFullscreen)
+		document.msExitFullscreen();
+}
+
+/**
+ * Check fullscreen status.
+ * @returns {number} is it fullscreen?
+ */
+function isFullScreen() {
+	return window.innerWidth / screen.width > 0.9 && window.innerHeight / screen.height > 0.9;
+}
+
+/**
+ * Returns current browser width.
+ * @returns {number} Current browser width.
+ */
+function windowInnerWidth() {
+	return window.innerWidth;
+}
+
+/**
+ * Returns current browser height.
+ * @returns {number} Current browser height.
+ */
+function windowInnerHeight() {
+	return window.innerHeight;
+}
+
+// - - - - - - - - - - Local Storage - - - - - - - - - -
+/**
+ * Load data stored on device. (string only)
+ * @param {string} loc Data location.
+ * @returns {string} Data received.
+ */
+ function loadData(loc) {
+	var dt = "";
+
+	if (isApp()===0){
+		var d=localStorage.getItem(loc)
+
+		if (d!=null)
+			dt=d;
+	} else
+		dt=App.loadData(loc);
+
+	return dt;
+}
+
+/**
+ * Save data to device.
+ * @param {string} loc Data location.
+ * @param {string} data Data to be saved.
+ */
+function saveData(loc,data) {
+	if (isApp() === 0)
+		localStorage.setItem(loc,data);
+	else
+		App.saveData(loc,data);
+}
+
+// - - - - - - - - - - Networking - - - - - - - - - -
+
+/**
+ * Connect to server.
+ */
+function networkOpen(){
+	network = new WebSocket(webSocketAddress);
+	network.addEventListener("open", function(){
+		events.push("connected");
+	});
+	network.addEventListener("message", function(e){
+		events.push(e.data);
+	});
+	network.addEventListener("error",function(){
+		events.push("error");
+		network.close();
+	});
+	network.addEventListener("close", function(){
+		events.push("close");
+	});
+}
+
+/**
+ * Get a network event.
+ */
+function networkReceive() {
+	if(events.length > 0)
+		return events.shift();
+	else
+		return "";
+}
+
+/**
+ * Send a message to the server.
+ * @param {string} data JSON string
+ */
+function networkSend(data) {
+	if(network !== null)
+		if(network.readyState === 1)
+			network.send(data);
+}
+
+// - - - - - - - - - - SysError99's WebApp check - - - - - - - - - -
+/**
+ * Is this app?
+ * @returns {number} Is is an application, or browser (0)
+ */
+ function isApp() {
+	if(typeof App !== "undefined")
+		return 1;
+	else 
+		return 0;
+}
+
+// - - - - - - - - - - Multi-Touch - - - - - - - - - -
+/**
+ * Start Touch API.
+ * @param {number} roomwidth Room width.
+ * @param {number} roomheight Room Height.
+ */
+function touchInvoke(roomwidth,roomheight) {
+    if (typeof touch.canvas === "undefined") {
+        touch.canvas = document.getElementById("canvas");
+        touch.canvas.addEventListener("touchstart", function(touchEvent) {
+            var rect = touch.canvas.getBoundingClientRect();
+
+            for (var touchIndex = 0; touchIndex < touchEvent.changedTouches.length; touchIndex++) {
+                var touchObj = touchEvent.changedTouches[touchIndex];
+
+                touches.push({
+                    id: touchObj.identifier,
+                    start: false,
+                    left: rect.left,
+                    top: rect.top,
+                    right: rect.right,
+                    bottom: rect.bottom,
+                    x: touchObj.clientX,
+                    y: touchObj.clientY,
+                    end: false,
+                    clean: false,
+                });
+            }
+
+            touchEvent.preventDefault();
+        }, false);
+        touch.canvas.addEventListener("touchmove", function(touchEvent) {
+            var rect = touch.canvas.getBoundingClientRect();
+
+            for (var touchIndex = 0; touchIndex < touchEvent.changedTouches.length; touchIndex++) {
+                var touchObj = touchEvent.changedTouches[touchIndex];
+
+                for (var activeTouchObjectIndex = 0; activeTouchObjectIndex < touches.length; activeTouchObjectIndex++) {
+                    if (touches[activeTouchObjectIndex].id === touchObj.identifier) {
+                        touches[activeTouchObjectIndex].left = rect.left;
+                        touches[activeTouchObjectIndex].top = rect.top;
+                        touches[activeTouchObjectIndex].right = rect.right;
+                        touches[activeTouchObjectIndex].bottom = rect.bottom;
+                        touches[activeTouchObjectIndex].x = touchObj.clientX;
+                        touches[activeTouchObjectIndex].y = touchObj.clientY;
+                        activeTouchObjectIndex = touches.length;
+                    }
+                }
+            }
+
+            touchEvent.preventDefault();
+        }, false);
+        touch.canvas.addEventListener("touchend", function(touchEvent) {
+            for (var touchIndex = 0; touchIndex < touchEvent.changedTouches.length; touchIndex++) {
+                var touchObj = touchEvent.changedTouches[touchIndex];
+
+                for (var activeTouchObjectIndex = 0; activeTouchObjectIndex < touches.length; activeTouchObjectIndex++) {
+                    if (touches[activeTouchObjectIndex].id === touchObj.identifier) {
+                        touches[activeTouchObjectIndex].end = true;
+                        break;
+                    }
+                }
+            }
+
+            touchEvent.preventDefault();
+        }, false);
+    }
+	touch.roomWidth = Math.floor(roomwidth);
+	touch.roomHeight = Math.floor(roomheight);
+}
+
+/**
+ * Retreive a touch event.
+ */
+function touchStart() {
+	var touchId = -1;
+
+	for (var touchIndex = 0; touchIndex < touches.length; touchIndex++) {
+		if (touches[touchIndex].start === false) {
+			touches[touchIndex].start = true;
+			touchId = touchIndex;
+			break;
+		}
+	}
+
+	return touchId;
+}
+
+/**
+ * Retreive a continuous touch event.
+ */
+function touchMove() {
+    if (touches.length > 0) {
+        if (touch.loop < touches.length - 1)
+            touch.loop++;
+        else
+            touch.loop = -1;
+    } else
+		touch.loop = -1;
+
+	return touch.loop;
+}
+
+/**
+ * Retreive a touch end event.
+ */
+function touchEnd() {
+	var touchId = -1;
+
+	for (var touchIndex = 0; touchIndex < touches.length; touchIndex++) {
+        var touchObj = touches[touchIndex];
+
+		if (touchObj.end === true && touchObj.clean === false) {
+            touchObj.clean = true;
+			touchId = touchIndex;
+			break;
+		}
+	}
+
+	return touchId;
+}
+
+/**
+ * Retreive X-coordinate of a touch.
+ * @param {number} touchId ID of a touch.
+ * @returns {number} X-coordinate of a touch.
+ */
+function touchX(touchId) {
+    var touchObj = touches[touchId];
+    var touchResult = -1;
+
+	if (typeof touchObj !== "object")
+        touchObj = null;
+
+    if (touchObj !== null)
+        touchResult = Math.floor((touchObj.x - touchObj.left) / (touchObj.right - touchObj.left) * touch.roomWidth);
+
+    return touchResult;
+}
+
+/**
+ * Retreive Y-coordinate of a touch.
+ * @param {number} touchId ID of a touch.
+ * @returns {number} Y-coordinate of a touch.
+ */
+function touchY(touchId) {
+	var touchObj = touches[touchId];
+    var touchResult = -1;
+
+	if (typeof touchObj !== "object")
+        touchObj = null;
+
+    if (touchObj !== null)
+        touchResult = Math.floor((touchObj.y - touchObj.top) / (touchObj.bottom - touchObj.top) * touch.roomHeight);
+
+    return touchResult;
+}
+
+/**
+ * Cleanup touch API.
+ */
+function touchClean() {
+    for (var touchIndex = 0; touchIndex < touches.length; touchIndex++) {
+        var touchObj = touches[touchIndex];
+
+		if (touchObj.clean === true) {
+            touches.splice(touchIndex,1);
+            touchIndex--;
+        }
+    }
+}
